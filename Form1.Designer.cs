5b185010704eab6b5a7de29ba41394a18a4899ac
Shoukox <58671746+Shoukox@users.noreply.github.com>
2020-10-10 18:57:05 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace besteFarben
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFarbe = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxFarbe
+            // 
+            this.textBoxFarbe.Font = new System.Drawing.Font("Segoe UI Semibold", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.textBoxFarbe.Location = new System.Drawing.Point(4, 12);
+            this.textBoxFarbe.Multiline = true;
+            this.textBoxFarbe.Name = "textBoxFarbe";
+            this.textBoxFarbe.Size = new System.Drawing.Size(208, 35);
+            this.textBoxFarbe.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(218, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Подобрать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.AutoSize = true;
+            this.textBoxResult.ForeColor = System.Drawing.Color.DarkGreen;
+            this.textBoxResult.Location = new System.Drawing.Point(12, 60);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(36, 13);
+            this.textBoxResult.TabIndex = 3;
+            this.textBoxResult.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(314, 160);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxFarbe);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFarbe;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label textBoxResult;
+    }
+}
+
